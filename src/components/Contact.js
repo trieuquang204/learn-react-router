d0042e9f66048756e7d280bc6e763b19b7f79ed2
trieuquang <trieuquang071997@gmail.com>
2020-12-22 17:19:36 +0700

    78 doi tuong prompt react-router-dom

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,22 @@
-import React, { component } from "react";
+import React from "react";
+import { Prompt } from "react-router-dom";
 
 function Contact() {
-  return <div className="App">Day la Contact page</div>;
+  return (
+    <div className="container">
+      Day la Contact page
+      <button type="button" className="btn btn-info d-block">
+        Cho phep
+      </button>
+      <button type="button" className="btn btn-danger d-block">
+        Khong cho phep
+      </button>
+      <Prompt
+        when={true}
+        message={(location) => `ban chac chan muon toi ${location.pathname}`}
+      />
+    </div>
+  );
 }
 
 export default Contact;
